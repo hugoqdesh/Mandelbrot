87ba6f35514d8f3cd4aa850aa0c12cec20ea9370
hugoqdesh <gitjohan5@gmail.com>
2025-10-29 18:48:53 +0200

    basic formula

--- src/main/java/lol/hugoqdesh/Main.java
+++ src/main/java/lol/hugoqdesh/Main.java
@@ -2,6 +2,39 @@ package lol.hugoqdesh;
 
 public class Main {
     static void main() {
-        System.out.println("Hello World");
+        int width = 800;
+        int height = 600;
+        int n = 255;
+
+        for(int i = 0; i < height; i++) {
+            double y = (i / 600.0) * 4 - 2;
+
+            for(int j = 0; j < width; j++) {
+                double x = (j / 800.0) * 4 - 2;
+
+                double zImaginary = 0;
+                double zReal = 0;
+
+                int count = 0;
+                while (count < n) {
+                    double cReal = x;
+                    double cImaginary = y;
+
+                    double tempReal = zReal * zReal - zImaginary * zImaginary + cReal;
+                    double tempImaginary = 2 * zReal * zImaginary + cImaginary;
+
+                    zReal = tempReal;
+                    zImaginary = tempImaginary;
+
+                    if (zReal * zReal + zImaginary * zImaginary > 4) {
+                        break;
+                    }
+
+                    count++;
+                }
+
+                System.out.println("x: " + x + " y: " + y);
+            }
+        }
     }
 }
